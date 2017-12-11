0beb5c3ea4f87f51406d42efee5ee696d6658500
Rūdolfs Blaumanis <Rudolfs.Blaumanis@testdevlab.com>
2017-12-11 20:52:39 +0200

    first version
    first version

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+              function initMap() {
+                var uluru = {lat: 56.951057, lng: 24.100637};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                  zoom: 15,
+                  center: uluru
+                });
+                var marker = new google.maps.Marker({
+                  position: uluru,
+                  map: map
+                });
+              }
+
+              
+            
+    
