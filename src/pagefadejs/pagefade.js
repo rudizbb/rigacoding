c89fccfb1bc3b7ec252c7aaf1db46e1fed6a34bd
Rūdolfs Blaumanis <Rudolfs.Blaumanis@testdevlab.com>
2017-12-13 18:05:16 +0200

    removed pagefade from index

--- src/pagefadejs/pagefade.js
+++ src/pagefadejs/pagefade.js
@@ -1,14 +1,28 @@
+
+
 $(window).on( 'load', function () {
+	
 	setTimeout( function () {
 		$( '.pagefade' ).fadeIn( 600 );
 	}, 600 );
-
-	$( 'a[href]' ).click( function () {	
+	
+	$( 'a[href]' ).click( function (event) {	
+		if (event.currentTarget.href == 'index.html#myCarousel')
+			{
+				
+				
+			} 
+		
 		var url = this.href;
+			
+		
 		
 		$( 'body' ).fadeOut( 600 );		
 		setTimeout( function () {
+			
 			location.href = url;
+	//		console.log(location.href);
+		
 		}, 600 );
 		return false;
 	});
